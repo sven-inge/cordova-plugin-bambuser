245811eb9ff890a63974dc8d034b3b1a5af8ee23
Tom Sundstrom <office@tomsun.ax>
2018-10-03 16:55:44 +0300

    Perform postinstall tasks using Node.js modules instead of depending on system tools

---
+++ bin/install-sdk.js
@@ -0,0 +1,28 @@
+var https = require('https');
+var request = require('request');
+var path = require('path');
+var unzipper = require('unzipper');
+
+var sdks = {
+  android: {
+    url: 'https://dist.bambuser.com/android_sdk/libbambuser-android-0.9.12-170628.zip',
+  },
+  ios: {
+    url: 'https://dist.bambuser.com/ios_sdk/libbambuser-ios-0.9.17-180103.zip',
+  },
+};
+
+if (process.argv.length < 3) {
+  throw new Error('Please specify SDK');
+}
+
+var sdk = sdks[process.argv[2]];
+
+if (!sdk) {
+  throw new Error('Unknown SDK specified, valid options: ' + Object.keys(sdks).join(', '));
+}
+
+request = require('request');
+
+var stream = request({url: sdk.url, pool: new https.Agent({keepAlive: false})});
+stream.pipe(unzipper.Extract({path: path.join(__dirname, '..')}));
