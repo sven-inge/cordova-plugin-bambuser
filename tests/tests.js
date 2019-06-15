fa9ea570fb1930df10dd968d6b6e80d84291fb3b
Tom Sundstrom <office@tomsun.ax>
2019-06-15 20:51:20 +0300

    Add test suite based on Cordova Plugin Test Framework
    https://github.com/apache/cordova-plugin-test-framework

---
+++ tests/tests.js
@@ -0,0 +1,59 @@
+exports.defineAutoTests = function() {
+  describe('cordova-plugin-bambuser', function() {
+
+    it('is mounted', function() {
+      expect(window.bambuser.broadcaster).toBeDefined();
+      expect(window.bambuser.player).toBeDefined();
+    });
+
+    it('has broadcasting functions', function() {
+      const { broadcaster } = window.bambuser;
+      expect(broadcaster.hideViewfinder).toBeDefined();
+      expect(broadcaster.setApplicationId).toBeDefined();
+      expect(broadcaster.setCustomData).toBeDefined();
+      expect(broadcaster.setSendPosition).toBeDefined();
+      expect(broadcaster.setTitle).toBeDefined();
+      expect(broadcaster.showViewfinderBehindWebView).toBeDefined();
+      expect(broadcaster.startBroadcast).toBeDefined();
+      expect(broadcaster.stopBroadcast).toBeDefined();
+      expect(broadcaster.switchCamera).toBeDefined();
+    });
+
+    it('has playback functions', function() {
+      const { player } = window.bambuser;
+      expect(player.close).toBeDefined();
+      expect(player.hidePlayer).toBeDefined();
+      expect(player.loadBroadcast).toBeDefined();
+      expect(player.pause).toBeDefined();
+      expect(player.resume).toBeDefined();
+      expect(player.setApplicationId).toBeDefined();
+      expect(player.setAudioVolume).toBeDefined();
+      expect(player.showPlayerBehindWebView).toBeDefined();
+    });
+
+    it('should be able to show an idle viewfinder', function(done) {
+      document.addEventListener('deviceready', onDeviceReady, false);
+      function onDeviceReady() {
+        const { broadcaster } = window.bambuser;
+        broadcaster.setApplicationId('CORDOVA-PLUGIN-BAMBUSER-TESTSUITE', function() {
+          broadcaster.showViewfinderBehindWebView(function() {
+            done();
+          }, function(err) { expect(false).toBe(true, err); done(); });
+        }, function(err) { expect(false).toBe(true, err); done(); });
+      }
+    });
+
+    it('should be able to show an idle player', function(done) {
+      document.addEventListener('deviceready', onDeviceReady, false);
+      function onDeviceReady() {
+        const { player } = window.bambuser;
+        player.setApplicationId('CORDOVA-PLUGIN-BAMBUSER-TESTSUITE', function() {
+          player.showPlayerBehindWebView(function() {
+            done();
+          }, function(err) { expect(false).toBe(true, err); done(); });
+        }, function(err) { expect(false).toBe(true, err); done(); });
+      }
+    });
+
+  });
+};
