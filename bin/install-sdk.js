441a36dc1d6369b0b3010dfbb41e108fbf6dfdcd
Tom Sundstrom <office@tomsun.ax>
2019-12-03 07:15:04 +0200

    Update Android SDK to 0.9.18

--- bin/install-sdk.js
+++ bin/install-sdk.js
@@ -5,7 +5,7 @@ var unzipper = require('unzipper');
 
 var sdks = {
   android: {
-    url: 'https://dist.bambuser.com/android_sdk/libbambuser-android-0.9.17-190319.zip',
+    url: 'https://dist.bambuser.com/android_sdk/libbambuser-android-0.9.18-190703.zip',
   },
   ios: {
     url: 'https://dist.bambuser.com/ios_sdk/libbambuser-ios-0.9.24-191008.zip',
